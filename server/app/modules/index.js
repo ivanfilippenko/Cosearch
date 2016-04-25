40f7e9d7f58765cf1e8d701508eb7aa54602f57d
jjang16 <jjang16@illinois.edu>
2016-04-25 04:23:23 -0500

    changed collection result directory and added clear

--- server/app/modules/index.js
+++ server/app/modules/index.js
@@ -7,6 +7,7 @@ var getFunctions = require('./getNames').getFunctions;
 var parenSeparator = require('./separator').parenSeparator;
 var camelSeparator = require('./separator').camelSeparator;
 var fs = require('fs');
+var exec = require('exec');
 
 var collectionContent = '';
 var mappingData = [];
@@ -14,31 +15,33 @@ var mappingData = [];
 var counter = 0;
 
 module.exports = function() {
-  dir.readFiles('./projects', {
-    encoding: 'utf8',
-    match: /((.)+)\.java/i
-  }, function(err, content, next) {
-    if (err) throw err;
-    var className = getClass(content);
-    var functionNames = getFunctions(content);
+  exec(['rm', '-rf', '/tmp/meta_runner/build/collection-inv'], function() {
+    dir.readFiles('./projects', {
+      encoding: 'utf8',
+      match: /((.)+)\.java/i
+    }, function(err, content, next) {
+      if (err) throw err;
+      var className = getClass(content);
+      var functionNames = getFunctions(content);
       
-    for (var i = 0; i < functionNames.length; i++) {
-      collectionContent += camelSeparator(parenSeparator(functionNames[i].content + ' ' + className.content)) + '\n';
-      mappingData.push({
-        className: className.content,
-        content: functionNames[i].content,
-        lineNumber: functionNames[i].lineNumber,
-        fileIndex: counter
-      });
-    }
-    counter++;
-    next();
-  }, function(err, files) {
-    for (var i = 0; i < mappingData.length; i++) {
-      mappingData[i].fileName = files[mappingData[i].fileIndex];
-    }
-    fs.writeFileSync('./public/data/collection.dat', collectionContent, 'utf-8');
-    fs.writeFileSync('./public/data/mapping.json', JSON.stringify(mappingData), 'utf-8');
-    if (err) throw err;
+      for (var i = 0; i < functionNames.length; i++) {
+        collectionContent += camelSeparator(parenSeparator(functionNames[i].content + ' ' + className.content)) + '\n';
+        mappingData.push({
+          className: className.content,
+          content: functionNames[i].content,
+          lineNumber: functionNames[i].lineNumber,
+          fileIndex: counter
+        });
+      }
+      counter++;
+      next();
+    }, function(err, files) {
+      for (var i = 0; i < mappingData.length; i++) {
+        mappingData[i].fileName = files[mappingData[i].fileIndex];
+      }
+      fs.writeFileSync('/tmp/meta_runner/data/collection/collection.dat', collectionContent, 'utf-8');
+      fs.writeFileSync('./public/data/mapping.json', JSON.stringify(mappingData), 'utf-8');
+      if (err) throw err;
+    });
   });
 };
