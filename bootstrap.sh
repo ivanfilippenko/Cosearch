1b1027a61032be605676d0d7d785cdf0baa581b3
jjang16 <jjang16@illinois.edu>
2016-04-24 21:29:42 -0500

    added node.js to provision

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,7 @@ sudo apt-get install -y --force-yes python-software-properties
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 
-sudo apt-get install -y --force-yes g++ g++-4.8 git make wget libjemalloc-dev zlib1g-dev
+sudo apt-get install -y --force-yes g++ g++-4.8 git make wget libjemalloc-dev zlib1g-dev curl
+
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
